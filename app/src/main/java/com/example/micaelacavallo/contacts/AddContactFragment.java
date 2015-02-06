0a2bd5e06c1cff96c3fae37968c8f53e8e3587eb
micaela.cavallo <micaela.cavallo@globant.com>
2015-02-06 19:22:42 -0300

    As a user, I want to go to a Contact creation screen when pressing the “Add Contact” button

--- app/src/main/java/com/example/micaelacavallo/contacts/AddContactFragment.java
+++ app/src/main/java/com/example/micaelacavallo/contacts/AddContactFragment.java
@@ -1,15 +1,26 @@
 package com.example.micaelacavallo.contacts;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class AddContactFragment extends Fragment {
+    ImageButton mImageButtonPicture;
+    byte[] mByteArray;
+
+    private static int REQUEST_IMAGE_CAPTURE = 1;
 
     public AddContactFragment() {
     }
@@ -18,6 +29,27 @@ public class AddContactFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_add_contact, container, false);
+        mImageButtonPicture = (ImageButton) rootView.findViewById(R.id.image_button_picture);
+        mImageButtonPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            }
+        });
         return rootView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButtonPicture.setImageBitmap(imageBitmap);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            mByteArray = stream.toByteArray();
+        }
     }
 }
